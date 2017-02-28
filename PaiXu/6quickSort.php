3c3238dc059941853089d8b3f9e04aad4bee88fb
Wsykarl <wsy940822@163.com>
2017-02-28 16:43:02 +0800

    f

--- PaiXu/6quickSort.php
+++ PaiXu/6quickSort.php
@@ -6,19 +6,30 @@
  * Time: 15:50
  */
 function quickSort($arr){
+    //判断参数是否是一个数组
+    if(!is_array($arr)) return false;
+
+    //递归出口:数组长度为1，直接返回数组
+    $length=count($arr);
+    if($length<=1) return $arr;
+
     $flag = $arr[0];
     $count = count($arr);
     $left = $right = array();
     for ($i = 1;$i<$count;$i++){
         if($flag > $arr[$i]){
-            $left[] = $arr;
+            $left[] = $arr[$i];
         }else{
             $right[] = $arr[$i];
         }
-        $left = quickSort($left);
-        $right = quickSort($right);
     }
-    $arr = array_merge($left,$arr[0],$right);
-    return $arr;
+    //递归调用
+    $left=quickSort($left);
+    $right=quickSort($right);
+    //将所有的结果合并
+    return array_merge($left,array($arr[0]),$right);
 }
 $arr = array(23,34,15,16,36,3,5,14,7);
+//调用
+echo "<pre>";
+print_r(quickSort($arr));
